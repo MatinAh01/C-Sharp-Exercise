01ee435a3571a6ea7c2488f2ecc307a39f3fe058
MatinAh01 <mtnahmady01@gmail.com>
2023-07-26 19:32:24 +0330

    add new file

---
+++ convert mile to kilometers/convert mile to kilometers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the distance in mile ");
+            var km = ConvertMile();
+            Console.WriteLine(km);
+        }
+        static double ConvertMile()
+        {
+
+            var convert = Convert.ToDouble(Console.ReadLine()) * 1.6093;
+
+            return convert;
+        }
+    }
+}
