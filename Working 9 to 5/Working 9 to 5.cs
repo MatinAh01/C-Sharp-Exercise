00528da76d0cfea32feddb9ed86b469c395fcf63
MatinAh01 <mtnahmady01@gmail.com>
2023-08-16 20:00:59 +0330

    new exercise

---
+++ Working 9 to 5/Working 9 to 5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LiveCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Infos:");
+            var salary = new double[4];
+            for (int i = 0; i < salary.Length; i++)
+            {
+                salary[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            var regularHours = 17 - salary[0];
+            regularHours = regularHours * salary[2];
+            double overTime = 0;
+            if (salary[1] > 17)
+            {
+             overTime = (salary[1] - 17) * salary[2] * salary[3];   
+            }
+            var result = regularHours + overTime;
+            Console.WriteLine($"your salary is: ${result}");
+        }
+    }
+}
